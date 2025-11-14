96aa7f4654ea2f419f086d3ee9aaf6290ec20183
stefanomagagni <134522559+stefanomagagni@users.noreply.github.com>
2025-11-14 16:08:08 +0100

    Refactor certificate expiry check to return issuer info

--- app/checker.py
+++ app/checker.py
@@ -1,32 +1,49 @@
-import ssl, socket, datetime, json
+import ssl
+import socket
+from datetime import datetime
+import json
 
-def get_cert_expiry(domain):
-    hostname = domain.replace("https://", "").replace("http://", "").split("/")[0]
+def get_cert_info(hostname):
     context = ssl.create_default_context()
-    with socket.create_connection((hostname, 443), timeout=5) as sock:
-        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-            cert = ssock.getpeercert()
-    exp = datetime.datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
-    return exp
+    conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname)
+    conn.settimeout(5.0)
+    conn.connect((hostname, 443))
+    cert = conn.getpeercert()
+    conn.close()
+
+    # Estraggo i dati principali
+    issuer = dict(x[0] for x in cert['issuer'])
+    issued_by = issuer.get('organizationName', issuer.get('commonName', 'Sconosciuto'))
+
+    return {
+        "expires": datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z"),
+        "issuer": issued_by
+    }
 
 def check_domains(config_path="app/config.json"):
-    with open("config.json") as f:
+    with open(config_path) as f:
         conf = json.load(f)
+
     results = []
-    now = datetime.datetime.utcnow()
     for d in conf["domains"]:
+        url = d["url"].replace("https://", "").replace("http://", "").split("/")[0]
+        alert_days = d.get("alert_days", conf.get("notify_before_days", 15))
         try:
-            expiry = get_cert_expiry(d["url"])
-            days_left = (expiry - now).days
+            info = get_cert_info(url)
+            expires = info["expires"]
+            issuer = info["issuer"]
+            days_left = (expires - datetime.utcnow()).days
             results.append({
-                "domain": d["url"],
-                "expires": expiry.strftime("%Y-%m-%d"),
+                "domain": url,
+                "expires": expires.strftime("%Y-%m-%d"),
                 "days_left": days_left,
-                "alert": days_left < d["alert_days"]
+                "issuer": issuer,
+                "alert": days_left <= alert_days
             })
         except Exception as e:
             results.append({
-                "domain": d["url"],
+                "domain": url,
                 "error": str(e)
             })
+
     return results
